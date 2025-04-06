0b3016fe355efabf2c95d6cfbbd1766f961c1ca7
Isabel Lopes <isabelnsantos1144@gmail.com>
2025-04-06 19:20:29 -0300

    Primeiro commit: app de câmera funcional

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { IonButton, IonContent, IonHeader, IonImg, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { NgIf } from '@angular/common'; // ← importar aqui!
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonImg,
+    NgIf // ← adicionar aqui também!
+  ],
 })
 export class HomePage {
+  imageUrl: string | undefined;
+
   constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+    });
+
+    this.imageUrl = image.dataUrl;
+  }
 }
