9c6a792fecd397082257d0c9f51d25ffcdd5d302
Isabel Lopes <isabelnsantos1144@gmail.com>
2025-04-06 21:12:55 -0300

    Ajuste 3

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'app_android_camera',
-  webDir: 'www',
-  bundledWebRuntime: false
+  webDir: 'www'
 };
 
 export default config;
